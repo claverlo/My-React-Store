7f6d340482b6afb2d0fa59a54ee8502eeccf33e4
claverlo <leopac84@icloud.com>
2026-02-20 20:58:42 -0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ function App() {
     <BrowserRouter>
     <NavBar />
     <Routes>
+        <Route path='/' element={<Home />} />
         <Route path='/home' element={<Home/>} />
         <Route path='/about' element={<About/>} />
         <Route path='/catalog' element={<Catalog/>} />
