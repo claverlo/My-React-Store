1033b0db9733206992558f017fe5949b4faf89d0
claverlo <leopac84@icloud.com>
2026-02-20 19:45:51 -0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
 import './App.css';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
-import Catalog from './Pages/Catalog';
+import Catalog from './Pages/Catalog'
+import About from "./Pages/About";
+import Home from "./Pages/Home";
+import Admin from "./Pages/admin";
+import Contact from "./Pages/Contact";
+
 
 function App() {
     return (
-        <div>
-            <NavBar />
-            <Catalog></Catalog>
-            <Footer />
-        </div>
+    <BrowserRouter>
+    <NavBar />
+    <Routes>
+        <Route path='/home' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/catalog' element={<Catalog/>} />
+        <Route path='/admin' element={<Admin />}></Route> 
+        <Route path='/contact' element={<Contact />} />
+
+    </Routes>
+    <Footer />
+</BrowserRouter>
+
     )
 }
 
