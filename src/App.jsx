328e3804bd984ceda6465bed6f569ad0bd4ef04e
claverlo <leopac84@icloud.com>
2026-02-20 20:04:07 -0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-
-import './App.css';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import Catalog from './Pages/Catalog'
